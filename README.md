445f1e7e39f79e0e78d8b40490d6a43096c4bb26
Moulik Gaglani <moulikgaglani@gmail.com>
2017-10-31 03:00:29 -0400

    project complete

---
+++ README.md
@@ -0,0 +1,7 @@
+#Description
+Created a real-time chat room
+Connected client and server via Web Sockets
+Clients have the ability to write name and messages
+The name and message are sent to the Node.js server
+Server sends back status to the sender and updates the chat room
+Messages are stored using MongoDB
---
+++ server.js
@@ -0,0 +1,49 @@
+var mongo = require('mongodb').MongoClient,
+   client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
+  if(err) throw err;
+
+  client.on('connection', function(socket){
+
+    //console.log('Somebody has connected');
+    var col = db.collection('messages'),//collection
+        sendStatus = function(s){
+          socket.emit('status',s);
+        };
+
+    //Emit all the messages (with some limit) to the client whent they first connect
+    col.find().limit(7).sort({_id: 1}).toArray(function(err, result) {
+      if(err) throw err;
+
+      socket.emit('output', result);
+    });
+
+    //Wait for input
+    socket.on('input', function(data) {
+      //console.log(data);
+
+      //put the input into database
+      var name = data.name,
+          message = data.message
+          whitespacePattern = /^\s*$/;
+
+      if(whitespacePattern.test(name) || whitespacePattern.test(message)){
+        sendStatus('Name and Message is required.');
+        console.log('Invalid');
+      } else {
+        col.insert({name: name, message: message}, function(){
+          //Emit latest message to all clients
+          client.emit('output', [data]);
+          sendStatus({
+            message: "Message Sent",
+            clear: true
+          });
+          console.log('Inserted');
+        });
+      }
+
+    });
+  });
+
+});
