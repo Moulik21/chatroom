ddf438b22d466b913fbd08dd124220367df3afd0
Moulik Gaglani <moulikgaglani@gmail.com>
2017-10-31 13:42:10 -0400

    using mLab for db prepping for deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var mongo = require('mongodb').MongoClient,
-   client = require('socket.io').listen(8080).sockets;
+   client = require('socket.io').listen(8080).sockets,
+   url = process.env.MONGOLAB_URL;
 
-mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
+mongo.connect(url, function(err,db){
   if(err) throw err;
 
   client.on('connection', function(socket){
